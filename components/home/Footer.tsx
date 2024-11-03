a3a725a3fa82825a9d058fbbace9a0517112d1ed
donttal <3233795485@qq.com>
2024-11-03 16:34:48 +0800

    调整footer的样式

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -55,13 +55,15 @@ export default function Footer() {
 
   return (
     <footer className='w-full bg-[#15141A]'>
-      <div className='mx-auto flex min-h-[251px] max-w-pc flex-col items-center justify-between p-10 pb-5 lg:h-[180px] lg:flex-row lg:px-0 lg:pb-10'>
-        <div className='flex flex-col items-center lg:items-stretch'>
-          <h1 className='text-xl font-bold text-white lg:h-8 lg:text-[32px]'>{t('title')}</h1>
-          <h2 className='text-xs'>{t('subTitle')}</h2>
+      <div className='mx-auto flex min-h-[251px] max-w-pc flex-col items-center justify-between p-10 pb-5 lg:min-h-[180px] lg:flex-row lg:px-20'>
+        {/* 在大屏幕下左侧logo区域 */}
+        <div className='flex flex-col items-center lg:items-start'>
+          <h1 className='text-xl font-bold text-white lg:text-2xl'>{t('title')}</h1>
+          <h2 className='text-xs lg:text-sm'>{t('subTitle')}</h2>
         </div>
-        <div className='mt-5 flex flex-col items-center gap-y-5 lg:mt-0 lg:flex-row lg:items-stretch lg:gap-x-10'>
-          <div className='flex w-full flex-col gap-2'>
+        {/* 在大屏幕下右侧链接区域 */}
+        <div className='mt-8 flex flex-col items-center gap-y-8 lg:mt-0 lg:flex-row lg:items-start lg:gap-x-20'>
+          <div className='flex flex-col items-center gap-2 lg:items-start'>
             <h2 className='font-bold'>{t('support')}</h2>
             {SupportLinks.map((item) => (
               <a
@@ -76,13 +78,13 @@ export default function Footer() {
               </a>
             ))}
           </div>
-          <div className='grid grid-cols-2 gap-x-10 gap-y-5 lg:grid-cols-1 lg:gap-3'>
+          <div className='flex flex-col items-center gap-2 lg:items-start'>
             {INFO_LIST.map((item) => (
               <InfoLink key={item.href} href={item.href} title={item.title} />
             ))}
             <a
               href={`mailto:${CONTACT_US_EMAIL}`}
-              className='whitespace-nowrap text-xs hover:opacity-70 lg:text-sm'
+              className='text-xs hover:opacity-70 lg:text-sm'
               title={t('contactUs')}
               type='email'
             >
